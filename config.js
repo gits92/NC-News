0a4575258b1338823aef5a1d30964a98d024ccda
Sunny <ssharda92@gmail.com>
2017-11-09 16:20:46 +0000

    adding deployment to heroku

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 module.exports = {
   DB: {
     test: "mongodb://localhost/northcoders-news-api-test",
-    dev: "mongodb://localhost/northcoders-news-api"
+    dev:
+      "mongodb://ncnewsbe:northcoderssandeep@ds149855.mlab.com:49855/s-sharda-nc"
   },
   PORT: {
     test: 3090,
-    dev: 3000
+    dev: process.env.PORT
   }
 };
